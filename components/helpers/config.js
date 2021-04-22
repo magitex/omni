03d949075fdb52bdff742261e1c1182601fb3714
magitex <avik.rswebtech@gmail.com>
2021-04-22 16:57:48 +0530

    new

--- components/helpers/config.js
+++ components/helpers/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: 'http://166.62.36.174:8080/OmniKeyzRestAPI',
+    baseUrl: 'https://166.62.36.174:8080/OmniKeyzRestAPI',
     dashboardData: '/rs/v1/master/getDashboardDetails',
     auth: '/oauth/token',
 };
