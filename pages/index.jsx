6dd9d67715586d17c9d61e351ce44b5b9ad18e10
magitex <avik.rswebtech@gmail.com>
2021-04-26 19:11:27 +0530

    api cyt

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,12 +30,12 @@ const HomepageDefaultPage = () => {
             const token=await Helper.getToken();
             // setTimeout(async()=>{
 if(token)
-   //data= await Helper.dashboardData();
+   data= await Helper.dashboardData();
 
             // },1000)
         }
         const token=await Helper.getToken();
-        console.log("new token ",localStorage.getItem("token"));
+        console.log("new token11",localStorage.getItem("token"));
         data= await Helper.dashboardData();
         // console.log("123 token",data);
         const tempBanners =data && data.data.data.dashboardPanel.banners; 
