46f7cd6529cac071f2871fff8b690999fb76a6e3
magitex <avik.rswebtech@gmail.com>
2021-04-27 12:55:02 +0530

    hhhh

--- components/partials/homepage/home-default/HomeDefaultBanner.jsx
+++ components/partials/homepage/home-default/HomeDefaultBanner.jsx
@@ -78,7 +78,16 @@ const HomeDefaultBanner = () => {
         <div className="ps-home-banner ps-home-banner--1">
             <div className="ps-container">
                 <div className="ps-section__left">{mainCarouselView}</div>
-               
+                 <div className="ps-section__right">
+                    <Promotion
+                        link="/shop"
+                        image={promotion1 ? promotion1.image : null}
+                    />
+                    <Promotion
+                        link="/shop"
+                        image={promotion2 ? promotion2.image : null}
+                    />
+                </div>
             </div>
         </div>
     );
