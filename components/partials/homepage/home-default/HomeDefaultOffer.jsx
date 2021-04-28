7db164361566a4e6ac3a7f9e71d9961e71c8f490
magitex <avik.rswebtech@gmail.com>
2021-04-28 16:28:22 +0530

    jkjjhyhhjhbbjhgfccfgjjjslider

--- components/partials/homepage/home-default/HomeDefaultOffer.jsx
+++ components/partials/homepage/home-default/HomeDefaultOffer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect,useContext,useState } from 'react';
 import Link from 'next/link';
 import Slider from 'react-slick';
 import SkeletonProduct from '~/components/elements/skeletons/SkeletonProduct';
-import { carouselFullwidth } from '~/utilities/carousel-helpers';
+import { carouselStandard } from '~/utilities/carousel-helpers';
 import CountDownSimple from '~/components/elements/CountDownSimple';
 import ProductDealOfDay from '~/components/elements/products/ProductDealOfDay';
 import { generateTempArray } from '~/utilities/common-helpers';
@@ -42,7 +42,7 @@ const HomeDefaultOffer = ({ collectionSlug }) => {
                 <ProductDealOfDay product={item} key={item.id} />
             ));
             productItemsView = (
-                <Slider {...carouselFullwidth} className="ps-carousel outside">
+                <Slider {...carouselStandard} className="ps-carousel outside">
                     {slideItems}
                 </Slider>
             );
